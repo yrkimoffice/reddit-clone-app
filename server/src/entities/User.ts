787bb809ec98018306566c53ef280ca9982f74e9
yrkim <yr.kim@purple.io>
2023-01-12 02:32:49 +0900

    feat : entity 생성

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,20 +1,36 @@
-/*
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import {BeforeInsert, Column, Entity, Index, OneToMany} from "typeorm"
+import {IsEmail, Length} from "class-validator";
+import bcrypt from 'bcryptjs';
+import BaseEntity from "./Entity";
+import Post from "./Post";
+import Vote from "./Vote";
 
-@Entity()
-export class User {
+@Entity("users")
+export default class User extends BaseEntity {
 
-    @PrimaryGeneratedColumn()
-    id: number
+    @Index()
+    @IsEmail(undefined, {message: "이메일 주소가 잘못되었습니다."})
+    @Length(1, 255, {message: "이메일 주소는 비워둘 수 없습니다."})
+    @Column({unique: true})
+    email: String;
+
+    @Index()
+    @Length(3, 32, {message: "사용자 이름은 3자 이상이어야 합니다."})
+    @Column({unique: true})
+    username: string
 
     @Column()
-    firstName: string
+    @Length(6, 255, {message: "비밀번호는 6자리 이상이어야 합니다."})
+    password: string;
 
-    @Column()
-    lastName: string
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[]
 
-    @Column()
-    age: number
+    @OneToMany(() => Vote, (vote) => vote.user)
+    votes: Vote[]
 
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
 }
-*/
