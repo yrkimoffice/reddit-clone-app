02dfa4309f5630666a4f0edf9ce651da25a65a25
yrkim <yrkimoffice@gmail.com>
2023-02-08 20:36:30 +0900

    feat : 커뮤니티 리스트 생성

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -5,6 +5,7 @@ import {isEmpty} from "class-validator";
 import Sub from "../entities/Sub";
 import {AppDataSource} from "../data-source";
 import User from "../entities/User";
+import Post from "../entities/Post";
 
 const router = Router();
 
@@ -49,6 +50,25 @@ const createSub = async (req: Request, res: Response) => {
     }
 };
 
-router.post("/", userMiddleware, authMiddleware, createSub);
+const topSubs = async (req: Request, res: Response) => {
+    try {
+        const imageUrlExp = `COALESCE(s."imageUrn", 'https://www.gravatar.com/avatar?d=mp&f=y')`;
+        const subs = await AppDataSource
+            .createQueryBuilder()
+            .select(`s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`)
+            .from(Sub, "s")
+            .leftJoin(Post, "p", `s.name = p."subName"`)
+            .groupBy('s.title, s.name, "imageUrl"')
+            .orderBy(`"postCount"`, "DESC")
+            .limit(5)
+            .execute();
+        return res.json(subs);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error : "문제가 발생했습니다."});
+    }
+};
 
+router.post("/", userMiddleware, authMiddleware, createSub);
+router.get("//sub/tobSubs", topSubs);
 export default router;
